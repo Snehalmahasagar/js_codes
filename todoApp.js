54766e446dfff6e19f12888244ca45a664184a17
Snehal Ajit Mahasagar <snehalmahasagar@gmail.com>
2026-02-08 22:58:08 +0530

    Added the time taken functionality

--- todoApp.js
+++ todoApp.js
@@ -1,15 +1,17 @@
 const addBtn = document.getElementById("btn_task")
-const taskinout = document.getElementById("taskInput")
+const taskInput = document.getElementById("taskInput")
 const list = document.getElementById("list")
 
 //adding tasks
 addBtn.addEventListener("click" , function(){
-    const taskText = taskinout.value;
+    const taskText = taskInput.value;
     if(taskText === ""){
         alert("please add the task")
         return;
     }
+
     alert(`Your ${taskText} taks added to the list!`)
+    
 
 //tasks lits
 const li = document.createElement("li")
@@ -21,6 +23,13 @@ span.textContent = taskText;
 const complete = document.createElement("button")
 complete.textContent = "👍"
 
+//start time
+const startTimeStamp = document.createElement("small")
+const timeStamp = Date.now();
+const dateTime = new Date(timeStamp);
+const showTime = dateTime.toLocaleString();
+
+
 complete.addEventListener("click", function(){
     span.classList.toggle("completed")
     
@@ -28,37 +37,41 @@ complete.addEventListener("click", function(){
     
     })
 
+
 //delete button
 const delete_btn =  document.createElement("button")
 delete_btn.textContent = "❎"
-
+const endTimestamp = document.createElement("small")
+const endtime = Date.now();
+const enddate = new Date(endtime)
+const endShowTime = enddate.toLocaleString();
 delete_btn.addEventListener("click", function(){
+    alert(`your task deleted at ${endShowTime}`)
     li.remove();
-    alert(`successfully completed the ${taskText} at ${endshowTime}`)
 })
 
-const startTimeStamp = document.createElement("small")
-const timeStamp = Date.now();
-const dateTime = new Date(timeStamp);
-const showTime = dateTime.toLocaleString();
+//time taken
+const timeTaken = document.createElement("button")
+timeTaken.textContent = "⏱️"
+timeTaken.addEventListener("click", function(){
+    const totalTime = endShowTime - showTime;
+    alert(`time taken for the taks is ${totalTime}`)
+})
 
-startTimeStamp.textContent = `${taskText} created at ${showTime}`
-li.append(startTimeStamp);
 
-/*const timeBtn = document.createElement("button")
-timeBtn.textContent= showTime;
-timeBtn.addEventListener("click" , function(){
-    
-})*/
 
 //appending
 li.appendChild(span);
 li.appendChild(complete);
 li.appendChild(delete_btn);
+li.append(startTimeStamp);
+li.append(endTimestamp);
+li.append(timeTaken)
 
 list.appendChild(li);
 
 //clearing input
-taskinout.value= "";
+taskInput.value= "";
 })
+
 
